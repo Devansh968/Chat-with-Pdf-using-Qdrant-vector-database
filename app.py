7b3c158bfc0b5f4614ef8c53aa34f745161bbdb7
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2023-07-25 14:46:10 +0530

    commit
    no comments

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.vectorstores import Qdrant
+import qdrant_client
+from qdrant_client.http import models
+from qdrant_client import QdrantClient
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
@@ -12,7 +16,7 @@ from langchain.callbacks import get_openai_callback
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
+    st.header("PDF READER 💬")
     
     # upload file
     pdf = st.file_uploader("Upload your PDF", type="pdf")
@@ -33,9 +37,23 @@ def main():
       )
       chunks = text_splitter.split_text(text)
       
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      # create embeddings and  object out  of  it    so that  we  are able  perform  search on  it
+      embeddings = OpenAIEmbeddings(
+      model_name="sentence-transformers/all-mpnet-base-v2"
+     )
+      client = qdrant_client.QdrantClient(
+    "https://0770f5b9-586a-4307-9410-550e20a69f1e.us-east-1-0.aws.cloud.qdrant.io:6333",
+    api_key="jVhqP1jbL7IFqQ7cPh97gGIXj7BIfIjeicR_WbLXg5YZSpWdDualdA", 
+      )
+      client.recreate_collection(
+    collection_name="chunks",
+    vectors_config=models.VectorParams(size=100, distance=models.Distance.COSINE),
+     )
+
+      knowledge_base= Qdrant(
+    client=client, collection_name="chunks", 
+      embeddings =embeddings ,
+     )
       
       # show user input
       user_question = st.text_input("Ask a question about your PDF:")
@@ -44,9 +62,8 @@ def main():
         
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
+        response = chain.run(input_documents=docs, question=user_question)
+          
            
         st.write(response)
     
